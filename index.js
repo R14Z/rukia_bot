8992d0d95f6721b25918180e8b9aa01d21d767b2
Riaz <81258483+R14Z@users.noreply.github.com>
2022-07-25 22:38:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const { token } = process.env;
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+const { ClientRequest } = require('http');
+
+const client = Client({ intents: GatewayIntentBits.Guilds });
+client.commands = new Collection();
+
+const functionFolders = fs.readdirSync(`./src/functions`);
+for (const folder of functionFolders) {
+    const functionFilers = fs.readdirSync(`./src/functions/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of functionFiles) require(`./functions${folder}/${file}`)(client);
+}
